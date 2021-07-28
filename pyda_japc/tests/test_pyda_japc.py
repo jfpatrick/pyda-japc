9f1f3fae6755d97ee834647b1b538b5b45096bc3
Phil Elson <philip.elson@cern.ch>
2021-07-28 12:23:52 +0200

    Add CI which depends on the upstream ds-model implementation.

--- pyda_japc/tests/test_pyda_japc.py
+++ pyda_japc/tests/test_pyda_japc.py
@@ -1,26 +1,26 @@
 """
-High-level tests for the  package.
+High-level tests for the pyds-japc package.
 
 """
 
 import pyda_japc
 import jpype as jp
-import cmmnbuild_dep_manager as c
 
 import pytest
 
+import pyds_model._ds_model as _ds_model
 
-@pytest.fixture
-def jvm():
-    
-    c.Manager().start_jpype_jvm()
+
+def test_ds_model_works():
+    # TODO: This test can be removed once we are using the module
+    #  in our tests.
+    assert _ds_model.BasicType is not None
 
 
 @pytest.fixture
 def japc_value(jvm):
     _ = jvm
     
-
     cern = jp.JPackage('cern')
     return cern.japc.value
 
@@ -33,11 +33,6 @@ def test_value_descriptor(japc_value):
     SimpleParameterValueFactory = jp.JClass(
         "cern.japc.core.factory.SimpleParameterValueFactory")
     v = SimpleParameterValueFactory.newSimpleParameterValue(jp.JInt(32))
-    # v = japc_value.IntValue(32)
-    
-    print(v.)
-    assert str(v) == 'asdasd'
 
-
-
-
+    # TODO: This test can be removed once we are using JAPC properly.
+    assert str(v) == '(int:1) -> 32'
